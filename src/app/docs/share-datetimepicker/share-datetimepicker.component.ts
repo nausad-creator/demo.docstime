803baa790dec08d0f59c77393978844bf19b889e
Dell <userfirstlast2@gmail.com>
2021-06-24 17:24:18 +0530

    update

--- src/app/docs/share-datetimepicker/share-datetimepicker.component.ts
+++ src/app/docs/share-datetimepicker/share-datetimepicker.component.ts
@@ -2,9 +2,9 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DoCheck, EventEm
 import { HomeService } from 'src/app/home.service';
 import * as moment from 'moment';
 import { DocsService } from '../docs.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { ReferralReceived } from '../docs.interface';
-import { map } from 'rxjs/operators';
+import { catchError, map, take, tap } from 'rxjs/operators';
 @Component({
   selector: 'app-share-datetimepicker',
   templateUrl: './share-datetimepicker.component.html',
@@ -16,14 +16,30 @@ export class ShareDatetimepickerComponent implements OnInit, DoCheck {
   changes = '';
   @Input() from: string;
   @Output() updateView: EventEmitter<any> = new EventEmitter();
-  scheduledAccepted$: Observable<Array<ReferralReceived>>;
+  scheduledAccepted$: Observable<ReferralReceived[]>;
+  scheduled$: string[] = [];
+  data = {
+    page: '0',
+    facilityID: '0',
+    patientName: '',
+    referbydoctorName: '',
+    insuranceNames: '',
+    patientGender: '',
+    refercaseUrgent: '',
+    reasonIDs: '',
+    refercaseVisitTime: '',
+    refercaseStatus: '',
+    startDate: '',
+    endDate: '',
+    languageID: '1',
+    doctorID: this.service.getDocLocal() ? this.service.getDocLocal().doctorID : this.service.getDocSession().doctorID
+  };
   constructor(
     private service: HomeService,
     private docService: DocsService,
     private cd: ChangeDetectorRef,
     private differs: KeyValueDiffers
   ) {
-    // detect diff
     this.differ = this.differs.find({}).create();
   }
   ngOnInit(): void {
@@ -33,7 +49,6 @@ export class ShareDatetimepickerComponent implements OnInit, DoCheck {
   updateReferView = () => {
     this.updateView.emit();
   }
-
   jQueryEvent = () => {
     jQuery(() => {
       ($('#datetimepicker2') as any).datetimepicker({
@@ -41,40 +56,67 @@ export class ShareDatetimepickerComponent implements OnInit, DoCheck {
         timepicker: false,
         format: 'd/m/Y',
         formatDate: 'd/m/Y',
+        minDate: new Date(),
         inline: true,
         scrollMonth: false,
         scrollInput: false,
-        onChangeDateTime: this.detectChange
+        highlightedDates: this.scheduled$,
+        onChangeDateTime: this.detectChange,
+        onChangeMonth: this.changeMonth
       });
       const currentValue = ($('#datetimepicker2') as any).datetimepicker('getValue');
+      this.getReferrals(currentValue);
       this.getReferralScheduled(currentValue);
     });
   }
-  detectChange = (dp: Date) => {
+  changeMonth = (dp: Date) => {
     this.getReferralScheduled(dp);
     this.cd.markForCheck();
   }
-
-  getReferralScheduled = (date: Date) => {
-    const data = {
-      page: '0',
-      facilityID: '0',
-      patientName: '',
-      referbydoctorName: '',
-      insuranceNames: '',
-      patientGender: '',
-      refercaseUrgent: '',
-      reasonIDs: '',
-      refercaseVisitTime: '',
-      refercaseStatus: '',
-      startDate: moment(date).format('YYYY-MM-DD'),
-      endDate: moment(date).format('YYYY-MM-DD'),
-      languageID: '1',
-      doctorID: this.service.getDocLocal() ? this.service.getDocLocal().doctorID : this.service.getDocSession().doctorID
-    };
-    this.scheduledAccepted$ = this.docService.referralReceivedLists(JSON.stringify(data))
-      .pipe(map(res => res[0].data)) as Observable<Array<ReferralReceived>>;
+  detectChange = (dp: Date) => {
+    this.getReferrals(dp);
     this.cd.markForCheck();
+  }
+  getReferralScheduled = (date: Date) => {
+    this.data.refercaseStatus = 'Accepted';
+    this.data.startDate = moment(new Date()).format('YYYY-MM-DD');
+    this.data.endDate = moment(date).endOf('month').format('YYYY-MM-DD');
+    this.docService.referralReceivedLists(JSON.stringify(this.data))
+      .pipe(map(res => res[0].data), take(1), catchError(() => of([]))).subscribe((res: ReferralReceived[]) => {
+        if (res) {
+          this.scheduled$ = res.map((d: ReferralReceived) =>
+            `${moment(new Date(d.refercaseVisitDate)).format('DD/MM/YYYY')},
+            ${moment(d.refercaseVisitTime, 'h:mm:ss').format('LT')},
+            ${'xdsoft_highlighted_mint'}`);
+          ($('#datetimepicker2') as any).datetimepicker({
+            lang: 'en',
+            timepicker: false,
+            format: 'd/m/Y',
+            formatDate: 'd/m/Y',
+            inline: true,
+            scrollMonth: false,
+            scrollInput: false,
+            highlightedDates: this.scheduled$
+          });
+          this.cd.markForCheck();
+        }
+        if (!res) {
+          this.scheduled$ = [];
+          this.cd.markForCheck();
+        }
+      }, err => {
+        throw new Error(err);
+      });
+  }
+  getReferrals = (date: Date) => {
+    this.data.refercaseStatus = '';
+    this.data.startDate = moment(date).format('YYYY-MM-DD');
+    this.data.endDate = moment(date).format('YYYY-MM-DD');
+    this.scheduledAccepted$ = this.docService.referralReceivedLists(JSON.stringify(this.data))
+      .pipe(map(res => res[0].data
+        .filter(refer => refer.refercaseStatus === 'Pending' || refer.refercaseStatus === 'Accepted')),
+         take(1), tap(() => this.cd.markForCheck()),
+         catchError(() => of([]))) as Observable<ReferralReceived[]>;
   }
   ngDoCheck(): any {
     const change = this.differ.diff(this);
--- src/app/facility/share-datetimepicker/share-datetimepicker.component.ts
+++ src/app/facility/share-datetimepicker/share-datetimepicker.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DoCheck, EventEmitter, Input, KeyValueDiffers, OnInit, Output } from '@angular/core';
 import * as moment from 'moment';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { catchError, map, take, tap } from 'rxjs/operators';
 import { HomeService } from 'src/app/home.service';
 import { ReferralReceived } from '../docs.interface';
 import { FacilityService } from '../facility.service';
@@ -17,17 +17,32 @@ export class ShareDatetimepickerComponent implements OnInit, DoCheck {
   changes = '';
   @Input() from: string;
   @Output() updateView: EventEmitter<any> = new EventEmitter();
-  scheduledAccepted$: Observable<Array<any>>;
+  scheduledAccepted$: Observable<ReferralReceived[]>;
+  scheduled$: string[] = [];
+  data = {
+    facilityID: this.service.getFaLocal() ? this.service.getFaLocal().facilityID : this.service.getFaSession().facilityID,
+    patientName: '',
+    referbydoctorName: '',
+    insuranceNames: '',
+    patientGender: '',
+    refercaseUrgent: '',
+    reasonIDs: '',
+    refercaseVisitTime: '',
+    refercaseStatus: '',
+    startDate: '',
+    endDate: '',
+    languageID: '1',
+    doctorID: '0',
+    page: '0'
+  };
   constructor(
     private service: HomeService,
     private facilityService: FacilityService,
     private cd: ChangeDetectorRef,
     private differs: KeyValueDiffers
   ) {
-    // detect diff
     this.differ = this.differs.find({}).create();
   }
-
   ngOnInit(): void {
     this.jQueryEvent();
     this.service.updateDateTimeSchedule.subscribe(res => this.changes = res);
@@ -35,49 +50,76 @@ export class ShareDatetimepickerComponent implements OnInit, DoCheck {
   updateReferView = () => {
     this.updateView.emit();
   }
-
-  public jQueryEvent = () => {
+  jQueryEvent = () => {
     jQuery(() => {
       ($('#datetimepicker2') as any).datetimepicker({
         lang: 'en',
         timepicker: false,
         format: 'd/m/Y',
         formatDate: 'd/m/Y',
+        minDate: new Date(),
         inline: true,
         scrollMonth: false,
         scrollInput: false,
-        onChangeDateTime: this.detectChange
+        highlightedDates: this.scheduled$,
+        onChangeDateTime: this.detectChange,
+        onChangeMonth: this.changeMonth
       });
       const currentValue = ($('#datetimepicker2') as any).datetimepicker('getValue');
+      this.getReferrals(currentValue);
       this.getReferralScheduled(currentValue);
     });
   }
-  public detectChange = (dp: Date) => {
+  changeMonth = (dp: Date) => {
     this.getReferralScheduled(dp);
     this.cd.markForCheck();
   }
-  getReferralScheduled = (date: Date) => {
-    const data = {
-      facilityID: this.service.getFaLocal() ? this.service.getFaLocal().facilityID : this.service.getFaSession().facilityID,
-      patientName: '',
-      referbydoctorName: '',
-      insuranceNames: '',
-      patientGender: '',
-      refercaseUrgent: '',
-      reasonIDs: '',
-      refercaseVisitTime: '',
-      refercaseStatus: '',
-      startDate: moment(date).format('YYYY-MM-DD'),
-      endDate: moment(date).format('YYYY-MM-DD'),
-      languageID: '1',
-      doctorID: '0',
-      page: '0'
-    };
-    this.scheduledAccepted$ = this.facilityService.referralReceivedLists(JSON.stringify(data))
-    .pipe(map(res => res[0].data)) as Observable<Array<ReferralReceived>>;
+  detectChange = (dp: Date) => {
+    this.getReferrals(dp);
     this.cd.markForCheck();
   }
-  ngDoCheck(): any {
+  getReferralScheduled = (date: Date) => {
+    this.data.refercaseStatus = 'Accepted';
+    this.data.startDate = moment(new Date()).format('YYYY-MM-DD');
+    this.data.endDate = moment(date).endOf('month').format('YYYY-MM-DD');
+    this.facilityService.referralReceivedLists(JSON.stringify(this.data))
+      .pipe(map(res => res[0].data), take(1), catchError(() => of([]))).subscribe((res: ReferralReceived[]) => {
+        if (res) {
+          this.scheduled$ = res.map((d: ReferralReceived) =>
+            `${moment(new Date(d.refercaseVisitDate)).format('DD/MM/YYYY')},
+            ${moment(d.refercaseVisitTime, 'h:mm:ss').format('LT')},
+            ${'xdsoft_highlighted_mint'}`);
+          ($('#datetimepicker2') as any).datetimepicker({
+            lang: 'en',
+            timepicker: false,
+            format: 'd/m/Y',
+            formatDate: 'd/m/Y',
+            inline: true,
+            scrollMonth: false,
+            scrollInput: false,
+            highlightedDates: this.scheduled$
+          });
+          this.cd.markForCheck();
+        }
+        if (!res) {
+          this.scheduled$ = [];
+          this.cd.markForCheck();
+        }
+      }, err => {
+        throw new Error(err);
+      });
+  }
+  getReferrals = (date: Date) => {
+    this.data.refercaseStatus = '';
+    this.data.startDate = moment(date).format('YYYY-MM-DD');
+    this.data.endDate = moment(date).format('YYYY-MM-DD');
+    this.scheduledAccepted$ = this.facilityService.referralReceivedLists(JSON.stringify(this.data))
+      .pipe(map(res => res[0].data
+        .filter(refer => refer.refercaseStatus === 'Pending' || refer.refercaseStatus === 'Accepted')),
+         take(1), tap(() => this.cd.markForCheck()),
+         catchError(() => of([]))) as Observable<ReferralReceived[]>;
+  }
+  ngDoCheck(): void {
     const change = this.differ.diff(this);
     if (change) {
       change.forEachChangedItem((item: any) => {
